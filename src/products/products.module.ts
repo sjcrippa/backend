c51d9dd9f9a24d340002efd0751890b514be212d
Santiago Crippa <santicrippa@gmail.com>
2024-09-03 18:15:00 -0300

    Connecting with Prisma and adding crud

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, PrismaService],
 })
 export class ProductsModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.prismaService.product.create({
+      data: createProductDto,
+    });
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prismaService.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const productFound = await this.prismaService.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!productFound) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return productFound;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
