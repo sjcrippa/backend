9520270b8f3f93e3999c52b65e50006aa5f66f7d
Santiago Crippa <santicrippa@gmail.com>
2024-09-03 19:41:54 -0300

    Adding docs with Swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,17 +11,21 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a product.' })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Return all products.' })
   findAll() {
     return this.productsService.findAll();
   }
